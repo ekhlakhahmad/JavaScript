0ed83a81e5eb13a8e804d8c2b041e6026784282a
ekhlakhahmad <ekhlakhahmad95@gmail.com>
2023-10-03 23:04:31 +0530

    added some programs

---
+++ 30_Name_Print_Ten_Times_While_Loop.js
@@ -0,0 +1,11 @@
+document.write("<br>");
+
+
+document.write("<h2>While Loop</h2>");
+
+var i = 1;
+while(i <= 10){
+    document.write(i + " Ekhlakh Ahmad <br>");
+    i++;
+}
+
---
+++ 31_Counting_1_to_100_both_Order.js
@@ -0,0 +1,20 @@
+document.write("<br>");
+
+document.write("<h2>Print 1 to 100 Ascending Order</h2>");
+
+var a = 1;
+while(a <= 100){
+    document.write(a, "<br>");
+    a++;
+}
+
+document.write("<br>");
+
+document.write("<h2>Print 100 to 1 Descending Order</h2>");
+var b = 100;
+while(b >= 1){
+    document.write(b, "<br>");
+    b--;
+}
+
+document.write("<br>");
